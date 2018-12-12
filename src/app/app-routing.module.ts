3ee2754b5619018bb0bba1f3eaed2e9cfce90f9c
Casa <ram-mds@hotmail.com>
2018-12-11 19:00:38 -0500

    Primera subida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
+import { InicioComponent } from './inicio/inicio.component';
+import { PublicarComponent } from './publicar/publicar.component';
+import { MisPublicacionesComponent } from './mis-publicaciones/mis-publicaciones.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'publicar', component: PublicarComponent },
+  { path: 'mis-publicaciones', component: MisPublicacionesComponent }
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule,
+  ],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { PublicarComponent } from './publicar/publicar.component';
+import { MisPublicacionesComponent } from './mis-publicaciones/mis-publicaciones.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    InicioComponent,
+    PublicarComponent,
+    MisPublicacionesComponent
   ],
   imports: [
     BrowserModule,
